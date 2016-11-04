4dcea94333d0ce5066e624dab8328e6561d6ccb7
fanyongdong daben <fanyongdong@niu.com>
2016-11-04 11:41:39 +0800

    update

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ mkdir dist app/img app/dist &&
 
 /home/ffmpeg/ffmpeg -i  ../video/f-k-1.mp4 -r 25 ./app/img/src_%03d.jpg &&
 
-node --stack-size=32000 app/app.js -b 245,245,245,255 -w 1400 -g 1080 -f 25 &&  cp ./app/dist ../video
+node --stack-size=32000 app/app.js -b 245,245,245,255 -w 1400 -g 1080 -f 25 &&  tar zcvf ../video/bao.tar.gz ./app/dist 
 
 #2324
